b0192aa7236d083bc004eccb2c9d42d9cff7e7e0
blurpy <blurpy@2dfc9d0d-4039-0410-88f4-fb2fd6d6bb23>
2010-06-27 12:46:43 +0000

    Adding skeleton for kouchat v2

---
+++ v2/trunk/src/main/java/net/usikkert/kouchat/App.java
@@ -0,0 +1,13 @@
+package net.usikkert.kouchat;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
